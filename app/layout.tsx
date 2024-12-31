e5f4785c8404708a4da874ad9ccd7ff297002b0e
Maheen-Zubair <sanazubair071@gmail.com>
2024-12-31 19:42:33 +0500

    minor changes 2.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import { Josefin_Sans } from "@next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 const josefinSans = Josefin_Sans({
   subsets: ["latin"], 
   weight: ["400", "700"], 
