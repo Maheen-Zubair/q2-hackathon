44b741f48db9942a3f9778b48afd987250ea32dd
Maheen-Zubair <sanazubair071@gmail.com>
2025-01-18 23:21:48 +0500

    day-3(hackathon-3)

--- app/section1/page.tsx
+++ app/section1/page.tsx
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 "use client";
 import React, { useEffect, useState } from "react";
 import { client } from "@/sanity/lib/client";
-=======
-"use client"
-import React from "react";
->>>>>>> 456cccb6cbe58d0fd4f5f32bb2840f8022bec79a
 import Image from "next/image";
 import chair from "../../public/sofa promotional header.png";
 import lamp from "../../public/image 32.png";
 import { Swiper, SwiperSlide } from "swiper/react";
-<<<<<<< HEAD
 import { Navigation, Pagination, Autoplay, EffectFade } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -133,64 +127,6 @@ export default function Section1() {
               </div>
             </div>
           </div>
-=======
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-export default function Section1() {
-  
-  return (
-    <div >
-         <Swiper
-        modules={[Navigation, Pagination, Autoplay,]}
-        navigation
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 2500 }}
-        loop={true}
-      >
-      <SwiperSlide>
-      <div className="mainContainer mt-6  relative flex  items-center  justify-center bg-[#F2F0FF] h-full   w-screen">
-
-        <div className="subContainer sm:flex-row flex-col flex   bg-[#F2F0FF] h-full w-[90%] m-0 p-0">
-          <div className="part1 flex justify-start sm:pl-10 items-start mt-2 ">
-            <Image
-              src={lamp}
-              alt="lamp"
-              className=" sm:h-[250px] h-[150px] sm:w-[400px] w-[130px]  "
-            />
-          </div>
-          <div className="part2 flex flex-col md:gap-1 mt-4 sm:mt-0  justify-center">
-            <div className="font-semibold text-[10px] md:text-[14px] elg:text-[16px]  text-[#FB2E86]">
-              Best Furniture For Your Castle....
-            </div>
-            <div className="text-[32px] md:text-[36px] lg:text-[40px] clg:text-[48px] elg:text-[53px] elg:leading-[81.98px] tracking-[1.5%] font-bold">
-              New Furniture Collection Trends in 2020
-            </div>
-
-            <div className="font-semibold text-[14px] elg:text-[16px] md:leading-[28px] text-[#8A8FB9]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in
-              est adipiscing in phasellus non in justo.
-            </div>
-
-            <button className=" md:mt-6 mt-3 text-[12px] w-[120px] h-[28px] sl:w-[163px] sl:h-[50px] text-white bg-[#FB2E86] rounded-[2px] hover:bg-[#d73078]  sl:text-[17px] leading-[19.92px] tracking-[2%] hover:shadow-black hover:shadow-sm">
-              Add To Cart
-            </button>
-           
-          </div>
-          <div className="w-full  flex justify-center md:h-[689px] items-center">
-            <div className="part3 flex mt-8 mb-8  w-[300px] sm:h-[550px] sm:w-[550px] flex-col items-center justify-center ">
-              <Image
-                src={chair}
-                alt="chair"
-                className=" xxmd:w-[500px] h-34 elg:w-[706px] elg:h-[689px] lg:w-[580px] lg:h-[510px] md:w-[400px] md:h-[389px]  xxmd:pt-3 "
-              />
-            </div>
-          </div>
-        </div>
-        </div>
-
->>>>>>> 456cccb6cbe58d0fd4f5f32bb2840f8022bec79a
         </SwiperSlide>
         <SwiperSlide>
           <div className="mainContainer mt-6 relative flex items-center justify-center bg-[#F2F0FF] h-full w-screen">
@@ -210,13 +146,8 @@ export default function Section1() {
                   New Furniture Collection Trends in 2020
                 </div>
                 <div className="font-semibold text-[14px] elg:text-[16px] md:leading-[28px] text-[#8A8FB9]">
-<<<<<<< HEAD
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna
                   in est adipiscing in phasellus non in justo.
-=======
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Magna in est adipiscing in phasellus non in justo.
->>>>>>> 456cccb6cbe58d0fd4f5f32bb2840f8022bec79a
                 </div>
                 <button className="md:mt-6 mt-3 text-[12px] w-[120px] h-[28px] sl:w-[163px] sl:h-[50px] text-white bg-[#FB2E86] rounded-[2px] hover:bg-[#d73078] sl:text-[17px] leading-[19.92px] tracking-[2%] hover:shadow-black hover:shadow-sm">
                   Add To Cart
@@ -239,54 +170,7 @@ export default function Section1() {
             </div>
           </div>
         </SwiperSlide>
-<<<<<<< HEAD
       </Swiper>
     </div>
-=======
-        <SwiperSlide>
-          <div className="mainContainer mt-6 relative flex items-center justify-center bg-[#F2F0FF] h-full w-screen">
-            <div className="subContainer sm:flex-row flex-col flex bg-[#F2F0FF] h-full w-[90%] m-0 p-0">
-              <div className="part1 flex justify-start sm:pl-10 items-start mt-2">
-                <Image
-                  src={lamp}
-                  alt="lamp"
-                  className="sm:h-[250px] h-[150px] sm:w-[400px] w-[130px]"
-                />
-              </div>
-              <div className="part2 flex flex-col md:gap-1 mt-4 sm:mt-0 justify-center">
-                <div className="font-semibold text-[10px] md:text-[14px] elg:text-[16px] text-[#FB2E86]">
-                  Best Furniture For Your Castle....
-                </div>
-                <div className="text-[32px] md:text-[36px] lg:text-[40px] clg:text-[48px] elg:text-[53px] elg:leading-[81.98px] tracking-[1.5%] font-bold">
-                  New Furniture Collection Trends in 2020
-                </div>
-                <div className="font-semibold text-[14px] elg:text-[16px] md:leading-[28px] text-[#8A8FB9]">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Magna in est adipiscing in phasellus non in justo.
-                </div>
-                <button className="md:mt-6 mt-3 text-[12px] w-[120px] h-[28px] sl:w-[163px] sl:h-[50px] text-white bg-[#FB2E86] rounded-[2px] hover:bg-[#d73078] sl:text-[17px] leading-[19.92px] tracking-[2%] hover:shadow-black hover:shadow-sm">
-                  Add To Cart
-                </button>
-                {/* <Image
-                  src={star}
-                  alt="star"
-                  className="absolute md:bottom-10 md:w-[66px] w-[40px] bottom-4 right-[55%] left-[45%]"
-                /> */}
-              </div>
-              <div className="w-full flex justify-center lg:h-[689px] items-center">
-                <div className="part3 flex mt-8 mb-8 w-[300px] sm:h-[550px] sm:w-[550px]  flex-col items-center justify-center">
-                  <Image
-                    src={chair}
-                    alt="chair"
-                    className="xxmd:w-[500px] h-34 elg:w-[706px] elg:h-[689px] lg:w-[580px] lg:h-[510px] md:w-[400px] md:h-[389px] xxmd:pt-3"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </SwiperSlide>
-        </Swiper>
-      </div>
->>>>>>> 456cccb6cbe58d0fd4f5f32bb2840f8022bec79a
   );
 }
