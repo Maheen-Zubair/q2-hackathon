d1d210d8dbf12f853a53c06aafbfb1e20c49aac7
Maheen-Zubair <sanazubair071@gmail.com>
2025-02-06 01:04:05 +0500

    Final for MarketPlace

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,8 @@
 
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-// Apna Clerk Sign-Up URL yahan replace karein
-const CLERK_SIGN_UP_URL = "https://modest-leech-95.accounts.dev/sign-in"; 
+export default clerkMiddleware(() => {
 
-const isCartRoute = createRouteMatcher([""]);
-// const isAdminRoute = createRouteMatcher(["/admin-dashboard"]);
-
-export default clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
-
-  // Agar user sign-in nahi hai aur /cart route par hai, toh redirect karein
-  if (isCartRoute(req) && !userId) {
-    return new Response(null, {
-      status: 307,
-      headers: {
-        Location: CLERK_SIGN_UP_URL,
-      },
-    });
-  }
 });
 
 export const config = {
